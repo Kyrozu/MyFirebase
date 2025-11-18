210c2a0687de78c83129f0cba146aa8b056b972b
Kyrozu <nat80222@gmail.com>
2025-11-18 20:21:59 +0700

    CLOUDINARY

--- app/src/main/java/ky/paba/myfirebase/MainActivity.kt
+++ app/src/main/java/ky/paba/myfirebase/MainActivity.kt
@@ -1,24 +1,31 @@
 package ky.paba.myfirebase
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Adapter
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.SimpleAdapter
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.cloudinary.android.MediaManager
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     var dataProvinsi = ArrayList<daftarProvinsi>()
-    var data: MutableList<Map<String, String>> = ArrayList()
+    var data: MutableList<Map<String, Any>> = ArrayList()
 
     //    lateinit var lvAdapter: ArrayAdapter<daftarProvinsi>      // untuk arrayList
     lateinit var lvAdapter: SimpleAdapter   // untuk mutableList
@@ -27,6 +34,14 @@ class MainActivity : AppCompatActivity() {
     lateinit var _etIbuKota: EditText
     lateinit var _btnSimpan: Button
     lateinit var _lvData: ListView
+    lateinit var _ivUpload: ImageView
+
+
+    private var CLOUDINARY_CLOUD_NAME = "dsfakaa7m"
+    private var UNSINGED_UPLOAD_PRESET = "preset1"
+    private var selectedImageUri: Uri? = null
+    private var cameraImageUri: Uri? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +61,8 @@ class MainActivity : AppCompatActivity() {
         _btnSimpan = findViewById(R.id.btnSimpan)
         _lvData = findViewById(R.id.lvData)
 
+        _ivUpload = findViewById(R.id.ivUpload)
+
         // pakai arrayList
 //        lvAdapter = ArrayAdapter<daftarProvinsi>(
 //            this,
@@ -57,18 +74,110 @@ class MainActivity : AppCompatActivity() {
         lvAdapter = SimpleAdapter(
             this,
             data,
-            android.R.layout.simple_list_item_2,
-            arrayOf("Pro", "Ibu"),
-            intArrayOf(android.R.id.text1, android.R.id.text2)
+//            android.R.layout.simple_list_item_2   // layout bawaan android
+            R.layout.list_item_with_image,  // custom layout
+            arrayOf("Img", "Pro", "Ibu"),
+//            intArrayOf(android.R.id.text1, android.R.id.text2)    // layout bawaan android
+            intArrayOf(R.id.imgLogo, R.id.text1, R.id.text2)    // custom layout
         )
 
+        lvAdapter.setViewBinder { view, data, _ ->
+            if (view.id == R.id.imgLogo) {
+                val imgView = view as ImageView
+                val defaultImage = com.google.android.gms.base.R.drawable
+                    .common_google_signin_btn_icon_light
+
+                if (data is String && data.isNotEmpty()) {
+                } else {
+                    imgView.setImageResource(defaultImage)
+                }
+                return@setViewBinder true
+            }
+            false
+        }
+
         _lvData.adapter = lvAdapter
+
+        _lvData.setOnItemClickListener { adapterView, view, i, l ->
+            _etProvinsi.setText(data[i].get("Pro").toString())
+            _etIbuKota.setText(data[i].get("Ibu").toString())
+        }
+
+        _lvData.setOnItemLongClickListener { adapterView, view, i, l ->
+            val namaProvinsi: String = data[i]["Pro"].toString()
+            db.collection("tbProvinsi")
+                .document(namaProvinsi)
+                .delete()
+                .addOnSuccessListener {
+                    Log.d("Firebase", namaProvinsi + " Berhasil dihapus")
+                    readData(db)
+                }
+                .addOnFailureListener {
+                    Log.d("Firebase", it.message.toString())
+                }
+            true
+        }
 
         _btnSimpan.setOnClickListener {
             TambahData(db, _etProvinsi.text.toString(), _etIbuKota.text.toString())
         }
 
         readData(db)
+
+        val config: Map<String, String> =
+            mapOf("cloud_name" to CLOUDINARY_CLOUD_NAME, "upload_preset" to UNSINGED_UPLOAD_PRESET)
+
+        MediaManager.init(this, config)
+
+        _ivUpload.setOnClickListener {
+            showImagePickDialog()
+        }
+    }
+
+    private val pickImageFromGallery =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            uri?.let {
+                selectedImageUri = it
+                _ivUpload.setImageURI(it)
+            }
+        }
+
+    private val takePicture =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) {
+            if (it) {
+                selectedImageUri = cameraImageUri
+                _ivUpload.setImageURI(cameraImageUri)
+            }
+        }
+
+    private fun createImageUri(): Uri? {
+        val imageFile = File(
+            cacheDir,
+            "temp_image_${System.currentTimeMillis()}"
+        )
+        return FileProvider.getUriForFile(
+            this,
+            "$packageName.fileProvider",
+            imageFile
+        )
+    }
+
+    private fun showImagePickDialog() {
+        val options = arrayOf("Pilih dari Galeri", "Ambil Foto")
+
+        AlertDialog.Builder(this)
+            .setTitle("Pilih Gambar")
+            .setItems(options) { dialog, which ->
+                when (which) {
+                    0 -> pickImageFromGallery.launch("image/*")
+                    1 -> {
+                        createImageUri().let { uri ->
+                            cameraImageUri = uri
+                            takePicture.launch(uri)
+                        }
+                    }
+                }
+            }.show()
     }
 
     fun TambahData(db: FirebaseFirestore, provinsi: String, ibuKota: String) {
